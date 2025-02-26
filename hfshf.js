1d7d81fec568cd3d31ba68300ed0c36a0fbe9eb3
Umarolimjonov2009 <feruzashokirova1988@gmail.com>
2025-02-26 20:53:07 +0500

    Add files via upload

---
+++ hfshf.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+
+
+alert("Biz random raqam chiqarmoqchimiz: 🤫");
+let randomSon=Math.floor(Math.random()*90+10);
+
+
+alert("Random orqali chiqqan son: "+randomSon);
+
+
+confirm("Random sonni 2ga ko'paytirib bo'lmoqchimiz");
+
+alert(`Random orqali chiqqan son: ${randomSon}\n Random sonni 2ga ko'paytirdik: ${randomSon*2}\n
+   Random sonni 2ga bo'ldik: ${randomSon/2}\n Random sonni 2ga qoldiqli bo'ldik: ${randomSon%2} `)
